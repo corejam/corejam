2cebdcf60cea3727a98ec6d2130202e22ddd9057
Matthias von Bargen <matthias@corejam.io>
2020-10-06 11:51:03 +0200

    adjusted paths

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,23 +1,32 @@
 import path from "path";
 import os from "os";
+import execa from "execa";
 
-let mono = false;
-try {
-  const packageJson = require(process.cwd() + "/package.json")
-  if (Object.keys(packageJson).includes("workspaces")) {
-    mono = true;
+let mono = true;
+
+function monoCheck() {
+  try {
+    execa.sync("yarn", ["workspaces", "info"]);
+  } catch (e) {
+    mono = false;
   }
 }
-catch (e) { }
+monoCheck()
 
-const rootPath = mono ? ".." : os.tmpdir();
+/**
+ * If we are inside our mono repo for dev purpose we want the build packages
+ * to be created under <lernaRoot>/packages 
+ */
+const rootPath = mono ? path.resolve(__dirname + "/../../") : os.tmpdir();
 
-export const envRoot = process.cwd();
+
+export const envRoot = process.env.INIT_CWD ?? process.cwd();
 export const cliRoot = path.join(__dirname, "..");
 export const stencilRunner = path.resolve(rootPath, "stencil-runner");
 export const testRunner = path.resolve(rootPath, "test-runner");
 export const reactBindingsRoot = path.resolve(rootPath, "react-bindings");
 
+console.log(envRoot)
 /*
 console.log("cwd", process.cwd())
 console.log("mono", mono)
