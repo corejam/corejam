738b9bc83e252c47124eb7051513645ca59c4b6b
Matthias von Bargen <matthias@corejam.io>
2020-10-08 14:23:08 +0200

    adjusted static test

--- test/cypress/cypress/integration/generateApp/generateApp_spec.js
+++ test/cypress/cypress/integration/generateApp/generateApp_spec.js
@@ -2,12 +2,9 @@ describe("Check our generated app is accessible", function () {
   it("Can see test routes and click", function () {
     cy.visit("/")
     cy.contains("Welcome to corejam")
-    cy.contains("Components")
     cy.contains("Routes")
     cy.get("app-welcome").get("a").first().click()
-    cy.wait(1000)
-    cy.get("app-root")
-
+    cy.get("app-root").contains("Hi, this is Corejam World")
     //More
   });
 });
