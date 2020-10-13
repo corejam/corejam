9d142daad117c7a08ae633b00933e1c563bfe128
Matthias von Bargen <matthias@corejam.io>
2020-10-13 15:22:03 +0200

    revert dotenv-webpack and fix server handler

--- packages/cli/src/commands/apiServer.ts
+++ packages/cli/src/commands/apiServer.ts
@@ -6,13 +6,14 @@ import micro, { send } from "micro";
 const cors = require("micro-cors")({ origin: "http://localhost:3001" });
 
 export default async function run() {
+  const corejamServer = await CorejamServer();
+
   const handler = async (req: any, res: any) => {
     if (req.method === "OPTIONS") {
       return send(res, 200, "ok!");
     }
 
-    const server = await CorejamServer();
-    return server.createHandler({ path: "/api/graphql" })(req, res);
+    return corejamServer.createHandler({ path: "/api/graphql" })(req, res);
   };
 
   const devServer = () => cors(handler);
