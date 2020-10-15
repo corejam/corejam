02a7ac340b5131b2bfe8108c15f9276781d337cd
Patrick Haug <patrick@corejam.io>
2020-10-15 18:47:57 +0200

    fix create app test

--- test/cypress/cypress/integration/generateApp/generateApp_spec.js
+++ test/cypress/cypress/integration/generateApp/generateApp_spec.js
@@ -1,10 +1,10 @@
 describe("Check our generated app is accessible", function () {
   it("Can see test routes and click", function () {
-    cy.visit("/")
-    cy.contains("Welcome to corejam")
-    cy.contains("Routes")
-    cy.get("app-welcome").get("a").first().click()
-    cy.get("app-root").contains("Hi, this is Corejam World")
+    cy.visit("/_corejam");
+    cy.contains("Welcome to corejam");
+    cy.contains("Routes");
+    cy.get("app-welcome").get("a").first().click();
+    cy.get("app-root").contains("Hi, this is Corejam World");
     //More
   });
 });
