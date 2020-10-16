9f647175ff922d41d3dc3fa70fcbaba08080c66d
Patrick Haug <patrick@corejam.io>
2020-10-16 08:04:42 +0200

    index route in new app and edited test

---
+++ packages/cli/src/templates/plugin/app/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Component, h, Host } from "@stencil/core";
+
+@Component({
+  tag: "route-index",
+  shadow: true,
+})
+export class Hallo {
+  render() {
+    return (
+      <Host>
+        <corejam-box flex direction="col" items="center" p={6}>
+          <corejam-box mb={4}>
+            <corejam-type as="h2">Welcome to corejam</corejam-type>
+          </corejam-box>
+          <cj-pluginName></cj-pluginName>
+        </corejam-box>
+      </Host>
+    );
+  }
+}
