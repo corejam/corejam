2e9c9770cf19edbf3a3d33ff3bbd976c258bf79c
Matthias von Bargen <matthias@corejam.io>
2020-10-21 14:43:51 +0200

    remove unneded style tag

--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -1,30 +1,6 @@
 import Document from "next/document";
 import { renderToString } from "@corejam/plugin-dershop/web-components/hydrate";
 
-const Style = ({ style }) =>
-  style.map((inline, index) => {
-    if (inline.class)
-      return (
-        <style
-          key={index}
-          dangerouslySetInnerHTML={{
-            __html: `
-          ${inline.inner
-                .split(" ")
-                .map((sub) => "." + inline.class + " " + sub)
-                .join(" ")} 
-          
-        `,
-          }}
-        />
-      );
-    const regex = new RegExp(".hash", "g");
-    return (
-      <style key={index} sty-id={inline.id}>
-        {inline.inner.replace(regex, "")}
-      </style>
-    );
-  });
 export default class ShopDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await super.getInitialProps(ctx);
