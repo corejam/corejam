f6173fc716c52b8373c8b804a6e7820c5e53d9d8
Patrick Haug <patrick@corejam.io>
2020-10-23 14:51:01 +0200

    rm unused stuff

--- packages/cli/src/helpers/replaceInFile.ts
+++ packages/cli/src/helpers/replaceInFile.ts
@@ -1,6 +1,5 @@
 import replace from "replace-in-file";
-import { reactBindingsRoot, stencilRunner } from "../config";
-import { appendAsync } from "fs-jetpack";
+import { reactBindingsRoot } from "../config";
 
 export async function replaceWebComponentsImport(name: string) {
   replace.sync({
@@ -8,11 +7,4 @@ export async function replaceWebComponentsImport(name: string) {
     from: "@corejam/stencil-runner",
     to: name,
   });
-}
-
-export async function addEnvVars(staticBuild = false) {
-  if (process.env.DEPLOYMENT_URL) {
-    await appendAsync(stencilRunner + "/.env", `DEPLOYMENT_URL=${process.env.DEPLOYMENT_URL}`);
-  }
-  if (staticBuild) await appendAsync(stencilRunner + "/.env", `\nMODE=static`);
 }
