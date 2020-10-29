5d7d99224c0180668b86dec900f8e0124d279fa4
Matthias von Bargen <matthias@corejam.io>
2020-10-29 09:02:19 +0100

    Cleanup/small fixes (#170)
    * test ssr generate app
    
    * link categories and manufacturers in bootstrap
    
    * add manufacturer to allProducts query

--- test/cypress/cypress/integration/generateApp/generateApp_spec.js
+++ test/cypress/cypress/integration/generateApp/generateApp_spec.js
@@ -1,7 +1,23 @@
 describe("Check our generated app is accessible", function () {
   it("Can see index route route", function () {
-    cy.visit("/");
-    cy.contains("Welcome to corejam");
-    //More
+    cy.visit("/")
+    
+    //Make sure the SSR content matches the client visit
+    cy.request("/").its('body').then(html => {
+      cy.wrap(html).get("body").invoke("html").then(html => {
+        const staticBody = html;
+
+        cy.visit("/");
+        cy.contains("Welcome to corejam");
+
+        cy.get('body').invoke('html').then(html => {
+          expect(html).to.equal(staticBody)
+        })
+      });
+    })
+
+    //Make sure we are not rendering duplicate elements
+    cy.get("corejam-run-app").its("length").should("be.eq", 1)
+    cy.get("corejam-run-router").its("length").should("be.eq", 1)
   });
 });
