e5b399f2a446fd995874d770f373d8d8fc23ac2b
Patrick Haug <patrick@corejam.io>
2020-11-02 12:17:56 +0100

    fix create app again

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,5 +1,3 @@
-import path from "path";
-import os from "os";
 import execa from "execa";
 
 let isMono = true;
@@ -13,13 +11,5 @@ function monoCheck() {
 }
 monoCheck();
 
-/**
- * If we are inside our mono repo for dev purpose we want the build packages
- * to be created under <lernaRoot>/packages
- */
-const rootPath = isMono ? path.resolve(__dirname + "/../../") : os.tmpdir();
-
 export const envRoot = isMono ? process.cwd() : (process.env.INIT_CWD as string);
-export const cliRoot = path.join(__dirname, "..");
-export const reactBindingsRoot = path.resolve(rootPath, "react-bindings");
 export const mono = isMono;
