c80d159803af9146d9de5cfba52994c45bd973f9
Patrick Haug <patrick@corejam.io>
2020-11-02 12:38:46 +0100

    fix create app again (#186)

--- packages/cli/src/commands/generateApp.ts
+++ packages/cli/src/commands/generateApp.ts
@@ -1,27 +1,22 @@
 import kleur from "kleur";
-import path from "path";
 import ora from "ora";
 import logSymbol from "log-symbols";
 import replace from "replace-in-file";
 import { isYarn } from "is-npm";
 import jetpack from "fs-jetpack";
 import execa from "execa";
-import { cliRoot, mono } from "../config";
+import { mono } from "../config";
 
 export default async function createApp(name: string) {
   return new Promise(async (res: any) => {
     const spinner = ora(`Creating new Corejam application: ${name}`).start();
     const pluginRootPath = (mono ? process.env.INIT_CWD : process.cwd()) + "/" + name;
 
-    spinner.text = "Copy templates";
+    spinner.text = "Bootstrapping app";
 
-    await jetpack.copyAsync(path.resolve(cliRoot, "dist", "templates", "plugin"), pluginRootPath);
+    await jetpack.copyAsync(__dirname + "/../templates/plugin", pluginRootPath);
 
     const installProcess = execa(isYarn ? "yarn" : "npm", ["install"], { cwd: pluginRootPath });
-
-    spinner.stopAndPersist({ text: "Templates finished", symbol: logSymbol.success });
-    // Replace all placeholder
-    spinner.start("Putting all stuff in place");
 
     //@ts-ignore
     await replace({
@@ -36,10 +31,6 @@ export default async function createApp(name: string) {
       to: name.charAt(0).toUpperCase() + name.slice(1),
     });
 
-    spinner.stopAndPersist({ text: "Copy finished", symbol: logSymbol.success });
-    // Rename placeholder files
-    spinner.start("Moving words around");
-
     await jetpack.renameAsync(pluginRootPath + "/server/resolvers/db/faker/pluginName.ts", name + ".ts");
     await jetpack.renameAsync(pluginRootPath + "/server/resolvers/pluginName.ts", name + ".ts");
     await jetpack.renameAsync(pluginRootPath + "/server/schema/pluginName.graphql", name + ".graphql");
@@ -50,26 +41,18 @@ export default async function createApp(name: string) {
     await jetpack.renameAsync(pluginRootPath + "/app/store/pluginName.ts", name + ".ts");
     await jetpack.renameAsync(pluginRootPath + "/shared/pluginName.ts", name + ".ts");
 
-    spinner.stopAndPersist({ text: "Renaming finished", symbol: logSymbol.success });
-
-    spinner.start("Installing all dependencies");
+    spinner.text = "Installing all dependencies";
 
     installProcess.on("exit", async () => {
-      spinner.stopAndPersist({ text: "Dependencies installed", symbol: logSymbol.success });
+      spinner.stopAndPersist({ text: "Bootstrap finished.", symbol: logSymbol.success });
       spinner.succeed(kleur.green("Everything is ready to go"));
       console.log();
       console.log();
-      console.log(`Bootstrap of ${name} is finished.`);
-      console.log();
-      console.log(`We suggest: `);
-      console.log();
       console.log(`${kleur.bold("cd into " + kleur.bold(kleur.red(name)))}`);
       console.log();
-      console.log("and");
       console.log();
-      console.log(`${kleur.green(isYarn ? "yarn build" : "npm run build")}`);
+      console.log("and then");
       console.log();
-      console.log("then");
       console.log();
       console.log(`${kleur.green(isYarn ? "yarn dev" : "npm run dev")}`);
     });
