fa7be0a055adf69f9a5e3ec5b7e65ad4f6a90de5
Patrick Haug <patrick@corejam.io>
2021-01-17 11:14:11 +0100

    Really confused.

--- packages/run/app/store/runStore.ts
+++ packages/run/app/store/runStore.ts
@@ -1,7 +1,7 @@
 import { createStore } from "@stencil/store";
-import { createRouter, Router } from "@stencil/router";
+import { createRouter, Router } from "stencil-router-v2";
 
-export { href } from "@stencil/router";
+export { href } from "stencil-router-v2";
 
 export const { state, get, reset, set, onChange } = createStore({
   router: createRouter(),
