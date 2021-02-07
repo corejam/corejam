e4303886576d4ae84de4a7cac544762c43ee1eae
Matthias von Bargen <matthias@corejam.io>
2021-02-07 10:19:46 +0100

    register db

--- packages/base/src/PluginManager.ts
+++ packages/base/src/PluginManager.ts
@@ -1,4 +1,38 @@
+import { DBProvider } from "./db/ProviderInterface";
+
 const dataClients: Map<string, object> = new Map();
+
+const dbProviders: Map<string, DBProvider> = new Map();
+
+/**
+ * Get a DB connection provided by one of the corejam DB packages.
+ */
+export function getDb(db?: string): DBProvider {
+  if (!db) {
+    if (!process.env.DB_DRIVER) {
+      throw new Error("No database driver defined");
+    }
+    db = process.env.DB_DRIVER;
+  }
+
+  const dbProvider = dbProviders.get(db);
+
+  if (!dbProvider) {
+    throw new Error(`${db} is not registered yet. Missing DB package?`);
+  }
+
+  return dbProvider;
+}
+
+/**
+ * This is the global registration point for DB packages.
+ * Should be called during the package init.
+ */
+export function registerDBProvider(db: string, provider: DBProvider): DBProvider {
+  dbProviders.set(db, provider);
+
+  return provider;
+}
 
 /**
  * Check if we have the data client already initialised and return it.
