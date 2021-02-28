f4e027566348fe3368e72e64a055c77fbcb176b9
Patrick Haug <patrick@corejam.io>
2021-02-28 13:15:16 +0100

    adjusted theme test

--- packages/core-components/app/components/Theme/theme.spec.tsx
+++ packages/core-components/app/components/Theme/theme.spec.tsx
@@ -2,19 +2,57 @@ import { newSpecPage } from "@stencil/core/testing";
 import { UiBase } from "../UIBase/ui-base";
 import { Theme } from "./Theme";
 
-const theme = {
-  colors: { black: "#fff" },
-};
-const serialized = JSON.stringify(theme);
+it("should override default values", async () => {
+  const pageBefore = await newSpecPage({
+    components: [Theme, UiBase],
+    html: `
+            <corejam-ui-base></corejam-ui-base>
+        `,
+  });
 
-it("should render grid compoment", async () => {
+  const theme = {
+    colors: { black: "#fff" },
+  };
+
+  expect(pageBefore.doc.head.innerText).toContain("--cj-colors-black: #000");
+
   const { doc } = await newSpecPage({
     components: [Theme, UiBase],
     html: `
         <corejam-ui-base></corejam-ui-base>
-        <corejam-ui-theme theme=${serialized}></corejam-ui-theme>
+        <corejam-ui-theme theme=${JSON.stringify(theme)}></corejam-ui-theme>
     `,
   });
 
   expect(doc.head.innerText).toContain("--cj-colors-black: #fff");
+});
+
+it("should set new color value", async () => {
+  const theme = {
+    colors: { corejam: "golden" },
+  };
+  const { doc } = await newSpecPage({
+    components: [Theme, UiBase],
+    html: `
+          <corejam-ui-base></corejam-ui-base>
+          <corejam-ui-theme theme=${JSON.stringify(theme)}></corejam-ui-theme>
+      `,
+  });
+
+  expect(doc.head.innerText).toContain("--cj-colors-corejam: golden");
+});
+
+it("should set random user defined value", async () => {
+  const theme = {
+    agency: { corejam: "green" },
+  };
+  const { doc } = await newSpecPage({
+    components: [Theme, UiBase],
+    html: `
+            <corejam-ui-base></corejam-ui-base>
+            <corejam-ui-theme theme=${JSON.stringify(theme)}></corejam-ui-theme>
+        `,
+  });
+
+  expect(doc.head.innerText).toContain("--cj-agency-corejam: green");
 });
