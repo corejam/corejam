cc2af9e5e2f9d6103a523fcc875a07d8b9b5dfe2
Patrick Haug <patrick@corejam.io>
2021-03-02 19:51:12 +0100

    correct css var

--- packages/core-components/app/utils/transform/typography/family.ts
+++ packages/core-components/app/utils/transform/typography/family.ts
@@ -1,6 +1,6 @@
 export default {
   property: "font-family",
   transform(value) {
-    return `var(--cj-font-size-${value}, ${value})`;
+    return `var(--cj-font-family-${value}, ${value})`;
   },
 };
