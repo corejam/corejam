7abe8ffa6ebbca2c92735933d32ffaaa7d12703e
Patrick Haug <patrick@corejam.io>
2021-03-07 08:47:16 +0100

    hm

--- examples/nextjs/pages/_app.js
+++ examples/nextjs/pages/_app.js
@@ -1,14 +1,10 @@
-import { CorejamInit } from "@corejam/plugin-dershop/react";
 import Layout from "../components/Layout";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <CorejamInit router={useRouter()}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </CorejamInit>
   );
 }
 
